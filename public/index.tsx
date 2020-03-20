18f0a11f1324de95e4f823dab5d5571ba5e609d5
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-03-20 10:32:31 +0100

    feat(red-203): fixed redirect issues

--- public/index.tsx
+++ public/index.tsx
@@ -1,18 +1,36 @@
 import Core, { ModuleRouteConfig } from '@redactie/redactie-core';
 import React, { FC } from 'react';
+import { Redirect, useLocation } from 'react-router-dom';
 
 import { routes } from './lib/services/routes/routes.class';
-import { sitesRoutes } from './lib/sites.routes';
+import { SitesOverview } from './lib/views';
 
 const SitesComponent: FC<{ route: ModuleRouteConfig }> = ({ route }) => {
-	return <>{...sitesRoutes(route)}</>;
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const location = useLocation();
+
+	// if path is /sites, redirect to /sites/beheer
+	if (location.pathname === route.path) {
+		return <Redirect from={route.path} to={`${route.path}/beheer`} />;
+	}
+
+	return <>{Core.routes.render(route.routes as ModuleRouteConfig[])}</>;
 };
 
 // expose route
 Core.routes.register({
 	path: '/sites',
+	exact: true,
 	component: SitesComponent,
 	label: 'Sites',
+	isDefaultRoute: true,
+	routes: [
+		{
+			path: '/sites/beheer',
+			component: SitesOverview,
+			routes: [],
+		},
+	],
 });
 
 // expose module
