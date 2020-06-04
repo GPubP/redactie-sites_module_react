a0433ab2949be6f6a4bd9fc352768496724172ed
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-06-04 14:52:16 +0200

    feat: add RenderChildRoutes component

--- public/lib/components/index.ts
+++ public/lib/components/index.ts
@@ -2,3 +2,4 @@ export { default as DataLoader } from './DataLoader/DataLoader';
 export { default as SitesDetailForm } from './SitesDetailForm/SitesDetailForm';
 export { default as Status } from './Status/Status';
 export { default as SiteStatus } from './SiteStatus/SiteStatus';
+export { default as RenderChildRoutes } from './RenderChildRoutes/RenderChildRoutes';
