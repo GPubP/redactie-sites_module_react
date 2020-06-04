b6ead850228db25c05ba6a02b6bc67185e41347e
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-06-04 17:07:57 +0200

    feat: add x-tenant-id to key headers

--- public/lib/services/api/api.service.ts
+++ public/lib/services/api/api.service.ts
@@ -1,12 +1,18 @@
+import Core from '@redactie/redactie-core';
 import ky from 'ky';
 
 import { SearchParams } from './api.service.types';
 
 export type KyInstance = typeof ky;
 
+const CoreConfig = Core.config.getValue('core') || {};
+
 // Create ky instance with defaults
 const api: KyInstance = ky.create({
 	prefixUrl: '/v1/proxy/sites/v1',
+	headers: {
+		'x-tenant-id': CoreConfig.tenantId,
+	},
 });
 
 export const parseSearchParams = (searchParams: SearchParams): URLSearchParams => {
