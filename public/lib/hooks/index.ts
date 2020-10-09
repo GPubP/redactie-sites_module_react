0d731e39d674ebee2e7b3a797f95a573f4ae5885
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-10-09 13:41:24 +0200

    refactor: import usePrevious from utils

--- public/lib/hooks/index.ts
+++ public/lib/hooks/index.ts
@@ -1,6 +1,5 @@
 export { default as useSite } from './useSite/useSite';
 export { default as useSitesLoadingStates } from './useSitesLoadingStates/useSitesLoadingStates';
 export { default as useHomeBreadcrumb } from './useHomeBreadcrumb/useHomeBreadcrumb';
-export { default as usePrevious } from './usePrevious/usePrevious';
 export { default as useSitesPagination } from './useSitesPagination/useSitesPagination';
 export { default as useRolesRightsApi } from './useRolesRightsApi/useRolesRightsApi';
