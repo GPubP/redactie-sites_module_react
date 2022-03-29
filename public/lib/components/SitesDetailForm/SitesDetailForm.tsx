0c651d289ba29ba24d85c1ddd82e487cbeb44234
Lotje <lotte-moors@hotmail.com>
2022-03-29 08:41:20 +0200

    feat: add languageHeader + set url as multiLanguageField
    + update utils package , translations & language module

--- public/lib/components/SitesDetailForm/SitesDetailForm.tsx
+++ public/lib/components/SitesDetailForm/SitesDetailForm.tsx
@@ -7,11 +7,24 @@ import {
 	Modal,
 	TextField,
 } from '@acpaas-ui/react-components';
-import { ActionBar, ActionBarContentSection } from '@acpaas-ui/react-editorial-components';
-import { CopyValue, ErrorMessage, FormikOnChangeHandler } from '@redactie/utils';
+import {
+	ActionBar,
+	ActionBarContentSection,
+	LanguageHeader,
+} from '@acpaas-ui/react-editorial-components';
+import { LanguageSchema } from '@redactie/language-module';
+import {
+	CopyValue,
+	ErrorMessage,
+	FormikMultilanguageField,
+	FormikOnChangeHandler,
+	Language,
+} from '@redactie/utils';
 import { Field, Formik } from 'formik';
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import languagesConnector from '../../connectors/languages';
 import TranslationsConnector, { CORE_TRANSLATIONS } from '../../connectors/translations';
 import { SitesDetailFormState } from '../../sites.types';
 import SitesStatus from '../SiteStatus/SiteStatus';
@@ -34,6 +47,15 @@ const SitesDetailForm: FC<SitesDetailFormProps> = ({
 	onActiveToggle,
 }) => {
 	const [t] = TranslationsConnector.useCoreTranslation();
+	const { siteId } = useParams<{ siteId: string }>();
+	const [activeLanguage, setActiveLanguage] = useState<Language | LanguageSchema>();
+	const [languages] = languagesConnector.hooks.useActiveLanguagesForSite(siteId);
+
+	useEffect(() => {
+		if (Array.isArray(languages) && !activeLanguage) {
+			setActiveLanguage(languages.find(l => l.primary) || languages[0]);
+		}
+	}, [activeLanguage, languages]);
 
 	const getLoadingStateBtnProps = (
 		loading: boolean,
@@ -112,86 +134,92 @@ const SitesDetailForm: FC<SitesDetailFormProps> = ({
 	};
 
 	return (
-		<Formik
-			enableReinitialize
-			initialValues={initialState}
-			onSubmit={onSubmit}
-			validationSchema={SITES_DETAIL_VALIDATION_SCHEMA}
+		<LanguageHeader
+			languages={languages}
+			activeLanguage={activeLanguage}
+			onChangeLanguage={(language: string) => setActiveLanguage({ key: language })}
 		>
-			{formikProps => {
-				const { submitForm, resetForm } = formikProps;
-				return (
-					<>
-						<FormikOnChangeHandler
-							onChange={values => onChange(values as SitesDetailFormState)}
-						/>
-						<div className="row u-margin-bottom">
-							<div className="col-xs-12 col-md-8 row middle-xs">
-								<div className="col-xs-12 col-md-8">
-									<Field
-										description="Geef de site een korte en duidelijke naam. Deze naam verschijnt
+			<Formik
+				enableReinitialize
+				initialValues={initialState}
+				onSubmit={onSubmit}
+				validationSchema={SITES_DETAIL_VALIDATION_SCHEMA}
+			>
+				{formikProps => {
+					const { submitForm, resetForm } = formikProps;
+					return (
+						<>
+							<FormikOnChangeHandler
+								onChange={values => onChange(values as SitesDetailFormState)}
+							/>
+							<div className="row u-margin-bottom u-margin-top">
+								<div className="col-xs-12 col-md-8 row middle-xs">
+									<div className="col-xs-12 col-md-8">
+										<Field
+											description="Geef de site een korte en duidelijke naam. Deze naam verschijnt
 									in de applicatie."
-										as={TextField}
-										label="Naam"
-										name="name"
-										required
+											as={TextField}
+											label="Naam"
+											name="name"
+											required
+										/>
+										<ErrorMessage name="name" />
+									</div>
+								</div>
+							</div>
+							<div className="row u-margin-bottom">
+								<div className="col-xs-12 col-md-8 row middle-xs">
+									<div className="col-xs-12 col-md-8">
+										<FormikMultilanguageField
+											description="Locatie van de website."
+											asComponent={TextField}
+											label="URL"
+											name="url"
+											required
+										/>
+										{/* <ErrorMessage name="url" /> */}
+									</div>
+								</div>
+							</div>
+							{onActiveToggle ? renderArchive() : null}
+							{initialState.uuid && (
+								<div className="row u-margin-top">
+									<CopyValue
+										label="UUID"
+										value={initialState.uuid}
+										buttonText={t(CORE_TRANSLATIONS.GENERAL_COPY)}
+										className="col-xs-12"
 									/>
-									<ErrorMessage name="name" />
 								</div>
-							</div>
-						</div>
-						<div className="row u-margin-bottom">
-							<div className="col-xs-12 col-md-8 row middle-xs">
-								<div className="col-xs-12 col-md-8">
-									<Field
-										description="Locatie van de website."
-										as={TextField}
-										label="URL"
-										name="url"
-										required
-									/>
-									<ErrorMessage name="url" />
-								</div>
-							</div>
-						</div>
-						{onActiveToggle ? renderArchive() : null}
-						{initialState.uuid && (
-							<div className="row u-margin-top">
-								<CopyValue
-									label="UUID"
-									value={initialState.uuid}
-									buttonText={t(CORE_TRANSLATIONS.GENERAL_COPY)}
-									className="col-xs-12"
-								/>
-							</div>
-						)}
-						<ActionBar className="o-action-bar--fixed" isOpen>
-							<ActionBarContentSection>
-								<div className="u-wrapper row end-xs">
-									<Button onClick={() => onCancel(resetForm)} negative>
-										{t(CORE_TRANSLATIONS.BUTTON_CANCEL)}
-									</Button>
-									<Button
-										iconLeft={loading ? 'circle-o-notch fa-spin' : null}
-										disabled={loading || !isChanged}
-										className="u-margin-left-xs"
-										onClick={submitForm}
-										type="success"
-									>
-										{onActiveToggle
-											? t(CORE_TRANSLATIONS['BUTTON_SAVE'])
-											: t(CORE_TRANSLATIONS['BUTTON_SAVE-NEXT'])}
-									</Button>
-								</div>
-							</ActionBarContentSection>
-						</ActionBar>
-						{typeof children === 'function'
-							? (children as SitesDetailFormChildrenFn)(formikProps)
-							: children}
-					</>
-				);
-			}}
-		</Formik>
+							)}
+							<ActionBar className="o-action-bar--fixed" isOpen>
+								<ActionBarContentSection>
+									<div className="u-wrapper row end-xs">
+										<Button onClick={() => onCancel(resetForm)} negative>
+											{t(CORE_TRANSLATIONS.BUTTON_CANCEL)}
+										</Button>
+										<Button
+											iconLeft={loading ? 'circle-o-notch fa-spin' : null}
+											disabled={loading || !isChanged}
+											className="u-margin-left-xs"
+											onClick={submitForm}
+											type="success"
+										>
+											{onActiveToggle
+												? t(CORE_TRANSLATIONS['BUTTON_SAVE'])
+												: t(CORE_TRANSLATIONS['BUTTON_SAVE-NEXT'])}
+										</Button>
+									</div>
+								</ActionBarContentSection>
+							</ActionBar>
+							{typeof children === 'function'
+								? (children as SitesDetailFormChildrenFn)(formikProps)
+								: children}
+						</>
+					);
+				}}
+			</Formik>
+		</LanguageHeader>
 	);
 };
 
